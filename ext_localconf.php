4b128ce53bcb153b020446b5dce244ca54a5b2f3
Xippo <oliver.pfaff.email@gmail.com>
2017-03-10 14:30:12 +0100

    Changes made by Steffen: ADD template options for typoscript constants. Better folder structure. I push this before we lose the changes in nirvana, set on version 1.0.0 Beta, its now implemented in many projects seems working well.

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,3 +1,3 @@
 <?php
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:tgm_grids/Configuration/TypoScript/pageTsConfig.ts">');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:tgm_grids/Configuration/TSconfig/Page/gridelements.ts">');
